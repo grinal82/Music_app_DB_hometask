52bf0124c9d9bb17e1ffd81275c259cb11b06e85
girnal82 <grinal82@gmail.com>
2022-06-14 00:48:28 +0300

    track_duration modified to numeric

--- music_app_DB.sql
+++ music_app_DB.sql
@@ -31,7 +31,7 @@ create table if not exists Tracks (
 	id serial primary key,
 	album_id integer not null references Album(id),
 	track_name varchar(40) not null,
-	track_duration text
+	track_duration numeric(10,2)
 );
 
 create table if not exists Compilation (
@@ -45,10 +45,10 @@ create table if not exists Compilation_Track (
 	track_id integer references Tracks(id),
 	constraint pr_k primary key (compilation_id, track_id)
 );
-);
+
 
 create table if not exists Compilation_Track(
 compilation_id integer references Compilation(id),
 track_id integer references Tracks(id),
 constraints pk primary key (compilation_id, track_id)
-)
+);
